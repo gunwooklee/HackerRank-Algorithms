396323125e196da9235b76707e898f61a07004aa
Gun Wook Lee <gunwook.lee@finastra.com>
2020-11-14 17:08:59 -0600

    Create 001.js

---
+++ 001.js
@@ -0,0 +1,11 @@
+function simpleArraySum(ar) {
+    /* var sum = ar.reduce((a,b) => a+b);
+     return sum;
+     */
+    // or
+     var sum =0;
+     for (var i = 0; i < ar.length; i++) {
+     sum += ar[i];
+ }
+ return sum;
+ }
