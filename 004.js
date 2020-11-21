7eaba3e8aeb461b1d7f762cd4826145d5818e2e0
lekununk <54338065+lekununk@users.noreply.github.com>
2020-11-21 14:29:00 -0600

    004
    practise

--- 004.js
+++ 004.js
@@ -6,7 +6,7 @@ function compareTriplets(a, b) {
     var alice = 0;
     var bob = 0;
 
-    for (var i = 0; i < 3; i++){
+    for (var i = 0; i < 3; i++) {
         if (a[i] > b [i]) {
             alice += 1;
         } else if (a[i] < b[i]) {
