5db4cb085e2a514c4ec65ccb37579628f214b266
lekununk <54338065+lekununk@users.noreply.github.com>
2020-11-21 14:27:26 -0600

    Create 004.js

---
+++ 004.js
@@ -0,0 +1,18 @@
+/*
+Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+*/
+
+function compareTriplets(a, b) {
+    var alice = 0;
+    var bob = 0;
+
+    for (var i = 0; i < 3; i++){
+        if (a[i] > b [i]) {
+            alice += 1;
+        } else if (a[i] < b[i]) {
+            bob += 1;
+        }
+    }
+    var alibob = [alice, bob];
+    return alibob;
+}
