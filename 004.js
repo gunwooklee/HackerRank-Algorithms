7dc125fbd370fa03c3906434aaf507de084a77f8
lekununk <54338065+lekununk@users.noreply.github.com>
2020-11-21 14:41:05 -0600

    Update 004.js

--- 004.js
+++ 004.js
@@ -5,7 +5,6 @@ Alice and Bob each created one problem for HackerRank. A reviewer rates the two 
 function compareTriplets(a, b) {
     var alice = 0;
     var bob = 0;
-
     for (var i = 0; i < 3; i++) {
         if (a[i] > b [i]) {
             alice += 1;
