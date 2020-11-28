453759c8bb14bea56f9e596542156a2f8d1e0463
gunwooklee <gunwook.lee@finastra.com>
2020-11-28 16:33:31 -0600

    Create 011.js

---
+++ 011.js
@@ -0,0 +1,16 @@
+/*
+Complete the sockMerchant function in the editor below. It must return an integer representing the number of matching pairs of socks that are available.
+*/
+
+function sockMerchant(n, ar) {
+    var sortArray = ar.sort()
+    var pairArray = [];
+    var counts = 1;
+    for (var i = 0; i < n; i++){
+        if (sortArray[i] == sortArray[i+1]){
+            counts ++;
+        }
+    }
+    var result =  Math.ceil(counts/2)
+    return result;
+}
