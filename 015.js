407ad7fb1ceb3b12c44c313ebe338dc9029a5308
gunwooklee <gunwook.lee@finastra.com>
2020-12-04 19:47:30 -0600

    Create 015.js

---
+++ 015.js
@@ -0,0 +1,19 @@
+/*
+Number Line Jumps
+*/
+function kangaroo(x1, v1, x2, v2) {
+
+    if ((x1-x2)%(v2-v1) == 0) {
+        if (x1 > x2 && v1 > v2){
+            return "YES";
+        }  else if (x1 < x2 && v1 > v2) {
+            return "YES";
+        }   else if (x1 > x2 && v1 < v2) {
+            return "YES";
+        }  else if (x1 < x2 && v1 < v2) {
+            return "NO"
+        }
+    } else {
+        return "NO";
+    }
+}
