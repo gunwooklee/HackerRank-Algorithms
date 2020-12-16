b45ffaac4d4958951d97b59fb0b4dcb6e99d8b59
gunwooklee <gunwook.lee@finastra.com>
2020-12-16 16:38:06 -0600

    Create 018.js

---
+++ 018.js
@@ -0,0 +1,17 @@
+/*
+Between Two Sets
+*/
+
+function getTotalX(a, b) {
+    let validCount = 0;
+    
+    for (let x = 1; x <= 100; x++) {
+        if (a.every(int => (x % int == 0))) {
+            if (b.every(int => (int % x == 0))) {
+                validCount++;
+            }
+        }
+    }
+
+    return validCount;
+}
